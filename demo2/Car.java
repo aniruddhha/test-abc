ee404bc3b0c5d3daa6864dd10eaa8931874b1769
aniruddhha <aniruddha.kudalkar@gmail.com>
2023-03-21 11:34:01 +0530

    car class created

---
+++ demo2/Car.java
@@ -0,0 +1,14 @@
+package demo2;
+
+public class Car {
+    
+    private int speed;
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+}
